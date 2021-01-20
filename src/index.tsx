f8969b332ca851eecb03dfb7ae36ab37a92bcf0b
jasonblanchard <jas.blanchard@gmail.com>
2021-01-19 20:49:13 -0500

    swap order of accepts

--- src/index.tsx
+++ src/index.tsx
@@ -123,9 +123,7 @@ app.use('/session/authn*', csrfProtection, (request, response) => {
 
   const error = Boom.unauthorized();
 
-  console.log(request.headers.accept)
-  console.log({ accepts: request.accepts(['json', 'html']) })
-  switch (request.accepts(['html', 'json'])) {
+  switch (request.accepts(['json', 'html'])) {
     case 'html':
       return response.redirect(LOGIN_PATH);
     case 'json':
